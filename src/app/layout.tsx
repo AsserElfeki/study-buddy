7d98a9e0812d132ec5a156c9f68cf06c1fb9bea1
Asser Elfeki <assemou414@student.polsl.pl>
2023-08-05 15:23:30 +0200

    feat(ToDO.md): add initial to-do list for project tasks feat(package.json): add dependencies for next-auth, bcrypt, and vercel feat(prisma): add initial migration and schema for User model feat(prisma): add seed script to create initial admin user feat(public): add Icon.jpg and Icon.svg files feat(src/app/api): add API structure markdown file feat(src/app/api/auth/[...nextauth]): add route for authentication feat(src/app/api/session): add route to check session status feat(src/app/faq/page.tsx): add test code feat(src/app/layout.tsx): add NextAuthProvider component to wrap children in layout
    feat(page.tsx): add import statements for LoginButton, LogoutButton, ProfileButton, and RegisterButton to improve code organization and readability
    feat(page.tsx): add support for fetching server session using getServerSession() and authOptions to improve authentication functionality
    feat(page.tsx): add main container with styling to center and align content on the page
    feat(page.tsx): add container div with border and flex properties to display buttons and server session information
    feat(page.tsx): add LoginButton, RegisterButton, LogoutButton, and ProfileButton components to the container div
    feat(page.tsx): add h1 element to display "Server Session" heading
    feat(page.tsx): add pre element to display server session data
    feat(page.tsx): add User component to display client session data
    
    feat(register/page.tsx): create new file for register page component
    
    feat(buttons.tsx): create new file for button components
    feat(buttons.tsx): add Login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,29 @@
-import '@styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "@styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { NextAuthProvider } from "@components/providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Study In Poland - Portal',
-  description: 'Easily find and apply for study programmes in Poland from all around the world',
-}
+  title: "Study In Poland - Portal",
+  description:
+    "Easily find and apply for study programmes in Poland from all around the world",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-        {children}
-        </main>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+        <main className="app"><NextAuthProvider> {children } </NextAuthProvider></main>
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import {
+  LoginButton,
+  LogoutButton,
+  ProfileButton,
+  RegisterButton,
+} from "@/components/buttons";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { User } from '@components/user.component';
 
-const Home = () => {
-  return <div>hello world</div>;
-};
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "70vh",
+        width: "500px"
+      }}
+    >
+      <div className='flex flex-wrap mx-20 px-8 border-2 border-black'>
+        <LoginButton />
+        <RegisterButton />
+        <LogoutButton />
+        <ProfileButton />
 
-export default Home;
+        <h1>Server Session</h1>
+        <pre className='flex flex-wrap'>{JSON.stringify(session)}</pre>
+        <User />
+      </div>
+    </main>
+  );
+}
