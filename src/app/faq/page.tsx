09eaeec2e0bfe678b50e5fc89da015daaa41304f
Asser Elfeki <assemou414@student.polsl.pl>
2023-10-16 06:42:52 +0200

    !!

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -5,7 +5,7 @@ function Faq() {
     <div>
       <div className="w-screen bg-white my-2 py-10">
         <div className="w-10/12 lg:w-3/5 flex flex-col mx-auto">
-          <h1 className="text-xl md:text-3xl font-bold mb-4">
+          <h1 className="text-xl md:text-3xl font-bold mb-3">
             Frequently asked questions
           </h1>
           <p className="text-base md:text-lg ">
