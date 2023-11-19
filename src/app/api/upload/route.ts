f65bbb5822d0090bbae4a5ffb08c5136a3c81558
Asser Elfeki <assemou414@student.polsl.pl>
2023-11-19 13:53:10 +0100

    chore(route.ts): comment out unused code in the upload route file

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,6 +1,27 @@
-import { v2 as cloudinary } from "cloudinary";
+// import { v2 as cloudinary } from "cloudinary";
 
-// export async function GET(req, res) {
+// // export async function GET(req, res) {
+// //     const body = JSON.parse(req.body) || {};
+// //     const { paramsToSign } = body;
+
+// //     try {
+// //         const signature = cloudinary.utils.api_sign_request(
+// //             paramsToSign,
+// //             process.env.CLOUDINARY_API_SECRET
+// //         );
+// //         return Response.json({ signature },
+// //             {
+// //                 status: 200
+// //             }
+// //         );
+// //     } catch (error) {
+// //         res.status(500).json({
+// //             error: error.message,
+// //         });
+// //     }
+// // }
+
+// export async function POST(req, res) {
 //     const body = JSON.parse(req.body) || {};
 //     const { paramsToSign } = body;
 
@@ -20,24 +41,3 @@ import { v2 as cloudinary } from "cloudinary";
 //         });
 //     }
 // }
-
-export async function POST(req, res) {
-    const body = JSON.parse(req.body) || {};
-    const { paramsToSign } = body;
-
-    try {
-        const signature = cloudinary.utils.api_sign_request(
-            paramsToSign,
-            process.env.CLOUDINARY_API_SECRET
-        );
-        return Response.json({ signature },
-            {
-                status: 200
-            }
-        );
-    } catch (error) {
-        res.status(500).json({
-            error: error.message,
-        });
-    }
-}
